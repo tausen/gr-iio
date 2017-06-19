10348d420fcae8ed43cf7599ba9f62fffde4daf9
Mathias Tausen <mathias.tausen@gmail.com>
2017-06-19 22:56:55 +0200

    fmcomms2_source: make iio device names configurable
    Make it possible to handle cases where the ad9361-phy and
    cf-ad9361-lpc devices do not have their default names

--- lib/fmcomms2_source_impl.h
+++ lib/fmcomms2_source_impl.h
@@ -48,7 +48,8 @@ namespace gr {
 		    bool bbdc, const char *gain1, double gain1_value,
 		    const char *gain2, double gain2_value,
 		    const char *rf_port_select, const char *filter,
-		    bool auto_filter);
+		    bool auto_filter,
+		    const char *phyname, const char *rxname);
 
       void set_params(unsigned long long frequency,
 		      unsigned long samplerate, unsigned long bandwidth,
